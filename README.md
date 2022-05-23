fbbcd63bf5ab0d2015f3cbd64e39def02ac159ca
MichaelMontei <code-me@hotmail.com>
2022-05-23 16:56:15 +0200

    function made to get the right data out of the API, used literals to then insert inside the pokeContainer as a div.

--- README.md
+++ README.md
@@ -11,18 +11,20 @@
 - [X] Get to log only one pokemon in the console log
 	- Changed the API link to https://pokeapi.co/api/v2/pokemon/bulbasaur to get the data from 1 pokemon into the console log.
 
-- [X] Get the input from the user => when clicked on button show this in the poke_container.
+- [X] Get the input from the user => when clicked on button show this in the pokeContainer.
 	- Made a form in HTML to get input from user and button to search.
-	- Made an eventListener to get the user input value and print the value inside the poke_container.
+	- Made an eventListener to get the user input value and print the value inside the pokeContainer.
 	- ERROR: the page automatically refresh?
 
 - [X] Page automatically refreshes when the input shows on the poke_container
 	- Found something called preventDefault() that you can add on an eventListener to prevent this from happening.
 
 - [ ] Need to find a way how I can go through all pokemon and match the input field with that particular pokemon
-	- So far I found that you can use template literals ${} with the API to pass certain data and push that inside the poke_container.
+	- So far I found that you can use template literals ${} with the API to pass certain data and push that inside the pokeContainer.
 	- This part took me quite some time to figure out.
 	- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
 
 - [ ] Need a way to print the "template" containing (ID) (NAME) (IMG) (MOVES) (EVOLUTION) to the poke_container.
-	
+	- Used template literals to insert a complete Div element inside the pokeContainer div.
+	- Some pokemon have 80+ moves yikes ;) 
+	- also tested something with abilities.
--- index.js
+++ index.js
@@ -1,28 +1,32 @@
-const pokeContainer = document.getElementById('poke_container');
-// const poke_id = document.getElementById('poke-id');
+const pokeContainer = document.getElementById('stats');
 const pokemonButton = document.getElementById('pokemon-search-button');
-
+const url = `https://pokeapi.co/api/v2/pokemon/`;
 
 // async to fetch the API data in const pokemon
-const getPokemon = async id => {
-    const url = `https://pokeapi.co/api/v2/pokemon/bulbasaur`;
-    const res = await fetch(url);
-    const pokemon = await res.json();
-    console.log(pokemon);
+async function getPokemon () {
+    const value = document.getElementById('pokemon-search-input').value;
+    const data = await fetch(`${url}${value}`);
+    const response = await data.json();
+    console.log(response.name, response.id, response.sprites, response.abilities[0].ability.name, response.moves[0].move.name);
 
-    //eventListener when click on button it gets the value from the input and print on poke-container (page was refreshing constantly, so I added a preventDefault)
+//insert all the data into the querySelector, some pokemon have 80+ moves...
+
+    document.querySelector(".stats").innerHTML = `
+        <div id="stats">
+            <h3>Name: ${response.name}</h3><h3>ID: ${response.id}</h3>
+            <img src="${response.sprites.front_default}" alt="pokemon">
+            <h3>Abilities: ${response.abilities[0].ability.name}</h3>
+            <h3>Moves: ${response.moves[0].move.name}, ${response.moves[1].move.name}, ${response.moves[2].move.name}, ${response.moves[3].move.name}</h3>
+        </div>
+    `;
+}
+
+//eventListener when click on button it gets the value from the input and print on poke-container (page was refreshing constantly, so I added a preventDefault)
     pokemonButton.addEventListener("click", (e) =>{
         let value = document.getElementById('pokemon-search-input').value;
-
-        pokeContainer.innerText = value;
-        console.log(value);
         e.preventDefault();
         getPokemon();
     })
-
-
-}
-
 
 getPokemon();
 
