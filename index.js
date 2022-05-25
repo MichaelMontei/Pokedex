45c49a8f82b69ce8eaeb18e83329d271f74c082c
MichaelMontei <code-me@hotmail.com>
2022-05-25 17:02:43 +0200

    added function to show evolutions

--- index.js
+++ index.js
@@ -1,56 +1,84 @@
-const pokeContainer = document.getElementById('stats');
 const pokemonButton = document.getElementById('pokemon-search-button');
 const url = `https://pokeapi.co/api/v2/pokemon/`;
-const url2 = 'https://pokeapi.co/api/v2/pokemon-species/';
 
-// async to fetch the API data in const pokemon
+// async to fetch the API data in const pokemons
 async function getPokemon () {
-    const value = document.getElementById('pokemon-search-input').value;
+    let value = document.getElementById('pokemon-search-input').value;
     const data = await fetch(`${url}${value}`);
     const response = await data.json();
+    const evoData = await fetch (response.species.url);
+    const evoResponse = await evoData.json();
+    const chainData = await fetch (evoResponse.evolution_chain.url)
+    const chainResponse = await chainData.json();
+    const evolveToPokemon = chainResponse.chain.species.name;
+    const evolveToPokemonTwo = chainResponse.chain.evolves_to[0].species.name;
+    const evolveToPokemonThree = chainResponse.chain.evolves_to[0].evolves_to[0].species.name;
     console.log(response);
-    console.log(response.name, response.id, response.sprites, response.abilities[0].ability.name, response.moves[0].move.name, response.types[0].type.name);
+    console.log(evolveToPokemon);
+    console.log(evolveToPokemonTwo);
+    console.log(evolveToPokemonThree);
 
-    //had to rewrite this since som pokemon only have 1 move.
-    let name = document.querySelector(".name").innerHTML = "Name: "  + response.name;
-    let id = document.querySelector(".id").innerHTML = "ID: "  + response.id;
-    let type = document.querySelector(".types").innerHTML = "Types: "  + response.types[0].type.name;
-    let abilities = document.querySelector(".abilities").innerHTML = "Abilties: "  + response.abilities[0].ability.name;
+
+    const pokemonsArr = [];
+    pokemonsArr.push(evolveToPokemon);
+    pokemonsArr.push(evolveToPokemonTwo);
+    pokemonsArr.push(evolveToPokemonThree);
+
+
+    if(evolveToPokemon){
+        console.log("no evolutions found");
+    }else{
+
+    }
+    document.querySelector(".evolution").innerHTML = " ";
+    if(evolveToPokemonTwo) {
+        document.querySelector(".evolution").innerHTML += "Evolves into: " + evolveToPokemonTwo;
+    }else{
+
+    }
+
+    if(evolveToPokemonThree === evolveToPokemonThree){
+        document.querySelector(".evolution").innerHTML += "<br>Evolves into: " + evolveToPokemonThree;
+    }else{
+
+    }
+
+    //console.log(response);
+    //console.log(response.name, response.id, response.sprites, response.abilities[0].ability.name, response.moves[0].move.name, response.types[0].type.name);
+
+    //had to rewrite this since some pokemon only have 1 move.
+    document.querySelector(".name").innerHTML = "Name: " + response.name;
+    document.querySelector(".id").innerHTML = "ID: " + response.id;
+    document.querySelector(".types").innerHTML = "Types: " + response.types[0].type.name;
+    document.querySelector(".abilities").innerHTML = "Abilties: " + response.abilities[0].ability.name;
 
     addImage();
     moves();
 
 
-    function addImage(){
+    function addImage() {
         let pic = document.querySelector(".sprites");
         let img = new Image(200, 200);
         img.src = response.sprites.front_shiny;
-        pic.innerHTML ='';
+        pic.innerHTML = '';
         pic.appendChild(img);
     }
 
+
     //some Pokemons only have 1 move
-    function moves (){
+    function moves() {
         const move = [];
 
-        if (response.moves.length === 1){
+        if (response.moves.length === 1) {
             move.push(response.moves[0].move.name);
-            document.querySelector(".moves").textContent="Moves: " + move;
-        }else{
+            document.querySelector(".moves").textContent = "Moves: " + move;
+        } else {
             for (let i = 0; i < 4; i++) {
                 move.push(response.moves[i].move.name);
             }
-            console.log(move)
-            document.querySelector(".moves").textContent="Moves: " + move;
-        }}
-
-
-    // async function getEvolution () {
-    //     const evolutionData = await fetch(url2) + response.name;
-    //     const evolutionResponse = await evolutionData.json();
-    //     console.log(evolutionResponse);
-
-
+            document.querySelector(".moves").textContent = "Moves: " + move;
+        }
+    }
 
 
     // document.querySelector(".stats").innerHTML = `
@@ -62,17 +90,15 @@ async function getPokemon () {
     //         <p>Types: <h3>${response.types[0].type.name}</h3></p>
     //     </div>
     // `;
-}
 
+}
 //eventListener when click on button it gets the value from the input and print on poke-container (page was refreshing constantly, so I added a preventDefault)
-    pokemonButton.addEventListener("click", (e) =>{
+    pokemonButton.addEventListener("click", (e) => {
         let value = document.getElementById('pokemon-search-input').value;
         e.preventDefault();
         getPokemon();
 
-    })
-
-getPokemon();
+    });
 
 
 
